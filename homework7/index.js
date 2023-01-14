379c7afc30a218d5c5b8688b0835625a0a53fd7e
Julia.Polevska <polevska25@gmail.com>
2023-01-14 12:52:02 +0200

    added homework7

---
+++ homework7/index.js
@@ -0,0 +1,49 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+// ---------------------- Function 1 ------------------------------------
+
+function getMyTaxes(salary) {
+  return this.tax * salary;
+}
+
+console.log("getMyTaxes :", getMyTaxes.call(ukraine, 2000));
+console.log("************************************************************");
+
+// ---------------------- Function 2 ------------------------------------
+
+function getMiddleTaxes() {
+  return this.tax * this.middleSalary;
+}
+
+console.log("getMiddleTaxes - Ukraine :", getMiddleTaxes.call(ukraine));
+console.log("getMiddleTaxes - Latvia :", getMiddleTaxes.call(latvia));
+console.log("getMiddleTaxes - Litva :", getMiddleTaxes.call(litva));
+console.log("************************************************************");
+
+// ---------------------- Function 3 ------------------------------------
+
+function getTotalTaxes() {
+  return this.tax * this.middleSalary * this.vacancies;
+}
+
+console.log("getTotalTaxes - Ukraine :", getTotalTaxes.call(ukraine));
+console.log("getTotalTaxes - Latvia :", getTotalTaxes.call(latvia));
+console.log("getTotalTaxes - Litva :", getTotalTaxes.call(litva));
+console.log("************************************************************");
+
+// ---------------------- Function 4 ------------------------------------
+
+function getMySalary(country) {
+  const salary = Math.floor(Math.random() * (2000 - 1500 + 1) + 1500);
+  return {
+    salary,
+    taxes: country.tax,
+    profit: salary - country.tax,
+  };
+}
+
+setInterval(() => {
+  console.log("getMySalary :", getMySalary(ukraine));
+}, 10000);
