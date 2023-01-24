fce30c821b3104658b96dc22e65d23459ad1563c
Julia.Polevska <polevska25@gmail.com>
2023-01-24 21:58:46 +0200

    added condition for checking other letters

--- homework10/index.js
+++ homework10/index.js
@@ -2,18 +2,18 @@ const buttonsList = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 document.addEventListener("keydown", handleKeyDown);
 
 function handleKeyDown(e) {
-  playSound(e.key);
+  if (buttonsList.includes(e.key)) {
+    playSound(e.key);
 
-  const currentBtn = document.querySelector(`.${e.key}-btn`);
-  currentBtn?.focus();
+    const currentBtn = document.querySelector(`.${e.key}-btn`);
+    currentBtn?.focus();
 
-  const currentLetter = document.querySelector(".current-letter");
-  currentLetter.innerHTML = e.key.toUpperCase();
+    const currentLetter = document.querySelector(".current-letter");
+    currentLetter.innerHTML = e.key.toUpperCase();
+  }
 }
 
 function playSound(letter) {
-  if (buttonsList.includes(letter)) {
-    const audio = new Audio(`./wav/${letter}.wav`);
-    audio.play();
-  }
+  const audio = new Audio(`./wav/${letter}.wav`);
+  audio.play();
 }
