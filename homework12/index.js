11d4d8fdb6d19729e401c195ca8881bba365c130
Julia.Polevska <polevska25@gmail.com>
2023-02-13 22:48:00 +0200

    added homework12

---
+++ homework12/index.js
@@ -0,0 +1,66 @@
+async function fetchInfo(url = "") {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function fetchCharacters(characterUrls) {
+  const characterResponses = characterUrls.map(async (url) => {
+    return await (await fetch(url)).json();
+  });
+
+  return await Promise.all(characterResponses);
+}
+
+function renderHeader() {
+  return `<div class="header header-photo">Photo</div>
+          <div class="header">Name</div>
+          <div class="header">Birth Year</div>
+          <div class="header">Gender</div>`;
+}
+
+function renderCharacter(character) {
+  const randomNumber = Math.floor(Math.random() * (16 - 1 + 1) + 1);
+
+  return `<div class="character">
+            <img class="character-photo" src="./img/${randomNumber}.png" alt="Character photo">
+          </div>
+          <div class="character">${character.name}</div>
+          <div class="character">${character.birth_year}</div>
+          <div class="character">${character.gender}</div>`;
+}
+
+function renderCharacters(characters) {
+  return characters
+    .map((character, index) => {
+      return renderCharacter(character, index + 1);
+    })
+    .join("");
+}
+
+async function handleGetEpisodeInfo() {
+  const episodeNumber = +document.querySelector(".episode-input").value;
+
+  if (!isNaN(episodeNumber) && episodeNumber > 0 && episodeNumber <= 6) {
+    const episodeInfo = await fetchInfo(
+      `https://swapi.dev/api/films/${episodeNumber}`
+    );
+    const characters = await fetchCharacters(episodeInfo.characters);
+
+    const header = renderHeader();
+    const rows = renderCharacters(characters);
+
+    const characterTable = document.querySelector(".character-table");
+    characterTable.innerHTML = [header, rows].join("");
+  }
+}
+
+function handleNextButton() {
+  const episodeNumber = +document.querySelector(".episode-input").value;
+
+  if (!isNaN(episodeNumber) && episodeNumber > 0 && episodeNumber <= 6) {
+    window.location.href = window.location.href.replace(
+      "index.html",
+      `planets.html?episode=${episodeNumber}`
+    );
+  }
+}
---
+++ homework12/planets.js
@@ -0,0 +1,35 @@
+async function fetchInfo(url = "") {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function fetchPlanets(planetUrls) {
+  const planetResponses = planetUrls.map(async (url) => {
+    return await (await fetch(url)).json();
+  });
+
+  return await Promise.all(planetResponses);
+}
+
+function renderPlanets(planets) {
+  return planets
+    .map((planet, index) => {
+      return `<div class="planet">${planet.name}</div>`;
+    })
+    .join("");
+}
+
+async function handlePageLoad() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const episodeNumber = urlParams.get("episode");
+
+  const episodeInfo = await fetchInfo(
+    `https://swapi.dev/api/films/${episodeNumber}`
+  );
+  const planets = await fetchPlanets(episodeInfo.planets);
+
+  const rows = renderPlanets(planets);
+
+  const planetsTable = document.querySelector(".planets-table");
+  planetsTable.innerHTML = rows;
+}
