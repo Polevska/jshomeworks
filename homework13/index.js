5087648f4b0722d55ff3bdd64b1fa2d6ba8d8ad9
Julia.Polevska <polevska25@gmail.com>
2023-02-15 21:33:48 +0200

    added homework13

---
+++ homework13/index.js
@@ -0,0 +1,39 @@
+function* createIdGenerator(prevNumber = 0) {
+  const currentNumber = prevNumber + 1;
+  yield currentNumber;
+
+  yield* createIdGenerator(currentNumber);
+}
+
+const idGenerator = createIdGenerator();
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+
+console.log(
+  "--------------------------- Advanced ------------------------------------"
+);
+
+function* newFontGenerator(font) {
+  let currentFont = font;
+  while (true) {
+    let value = yield currentFont;
+    if (value === "up") {
+      currentFont += 2;
+    }
+    if (value === "down") {
+      currentFont -= 2;
+    }
+  }
+}
+
+const fontGenerator = newFontGenerator(14);
+
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
